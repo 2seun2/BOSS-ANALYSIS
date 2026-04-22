b884a695a60e3e800446cade7191604b51e86902
2seun2 <2seun2@gmail.com>
2026-04-22 12:31:18 +0900

    Create ​constants.py

---
+++ ​constants.py
@@ -0,0 +1,14 @@
+# 롯데케미칼 TDS 기반 (Tensile Strength @Yield, MPa)
+MATERIAL_DATA = {
+    "ABS (HG-0760)": {"yield_strength": 45, "modulus": 2300},
+    "PC (SC-1100)": {"yield_strength": 62, "modulus": 2400},
+    "PC/ABS (HAC-8250)": {"yield_strength": 55, "modulus": 2500},
+    "PA6 (KN-120)": {"yield_strength": 70, "modulus": 2800},
+    # ... 나머지 10종 추가
+}
+
+SCREW_DATA = {
+    "M2.0": {"d": 2.0, "p": 0.4},
+    "M2.6": {"d": 2.6, "p": 0.45},
+    "M3.0": {"d": 3.0, "p": 0.5},
+}
